ea1231a210f4c83cc9ebbea2681fdba6bd664c31
ArkadiuszRolnik <arkadiusz.rolnik@gmail.com>
2019-10-21 20:33:12 +0200

    Zmiany w skryptach

--- Napisy.py
+++ Napisy.py
@@ -6,5 +6,22 @@ napis2 = 'Ala ma kota'
 napis3 = 'A to jest "cudzysłów"'
 napis4 = "A to jest \"cudzysłów\" "
 
-dlugosc = len(napis1)
-print("Długość zmiennej napis1 to", dlugosc, "znaków")
+#dlugosc = len(napis1)
+#print("Długość zmiennej napis1 to", dlugosc, "znaków")
+
+#wiek = input("Podaj wiek:")
+#print("Twój wiek to",wiek.strip())
+s = "Ruda tańczy jak szalona"
+print(s.capitalize()) #kapitalki
+print(s.upper()) #w górę
+print(s.lower()) #w dół
+print(s.title()) #formatowanie jako tytuł
+print(s.swapcase()) #duże->małe, małe->duże
+print(s.center(100)) #centrowanie
+print(s.replace("R","D")) #zamiana znaków
+print("Czy liczba:",s.isdecimal()) #sprawdzenie czy string jest liczbą
+
+print("4-ta litera:", s[3]) #pobierz 4ta literę napisu, pierwsza to 0
+print("Litera przedostatnia:", s[-2]) #pobierz przedostatnią literę, ostatnia to -1
+s = "Hello!"
+print(s[0:5])
---
+++ string slice.py
@@ -0,0 +1,16 @@
+s = "Hello!"
+print(s[0:5]) #od indeksu 0 do 4 włącznie
+print(s[-3]) #3ci znak od końca
+
+s = "Ruda tańczy jak szalona"
+arr = s.split(" ")
+print(arr)
+print(arr[0]) #pierwszy element listy
+
+print(s[0:16:2]) #co 2gi znak od indeksu 0 do 15 włącznie
+print(s[::3]) #cały string co 3ci znak
+
+#Grande-finale
+#reverse w pythonie
+
+print(s[::-1])
