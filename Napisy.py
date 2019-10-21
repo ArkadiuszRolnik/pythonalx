56b6f5ef1af54d7f80d40493751d015613312e11
ArkadiuszRolnik <arkadiusz.rolnik@gmail.com>
2019-10-21 19:51:03 +0200

    Zmiany w skryptach

---
+++ Napisy.py
@@ -0,0 +1,10 @@
+#
+#Typ danych: napisy (albo string)
+#
+napis1 = "Ala ma kota"
+napis2 = 'Ala ma kota'
+napis3 = 'A to jest "cudzysłów"'
+napis4 = "A to jest \"cudzysłów\" "
+
+dlugosc = len(napis1)
+print("Długość zmiennej napis1 to", dlugosc, "znaków")
--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,5 @@
 #
 
 print("Ruda tańczy jak szalona")
+a=123.45
+print(a)
