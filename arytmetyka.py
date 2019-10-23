eb8f9636a9c25198d1c944c4866cf45e4a282af9
ArkadiuszRolnik <arkadiusz.rolnik@gmail.com>
2019-10-23 18:22:21 +0200

    Coś tu musi być

---
+++ arytmetyka.py
@@ -0,0 +1,14 @@
+#1. przypisz do zmiennych x i y jakies wartości liczbowe
+#2. korzystając z funkcji print wypisz na ekranie podstawowe działania artmetyczne(...)
+#3. uruchom program kilka razy dla różnych wartości x i y 
+
+x = 10
+y=15
+print("x=",x,"y",y)
+print("dodawanie",x+y)
+print("odejmowanie",x-y)
+print("dzielenie",x/y)
+print("mnożenie",x*y)
+print("dzielenie całkowite",x//y)
+print("reszta",x%y)
+print("potengowanie",x**y)
---
+++ typy_liczbowe.py
@@ -0,0 +1,20 @@
+# w pythonie mamy:
+# liczby całkowite - integer
+print(type(10))
+print(int("10",base=3))
+print(int(2.7))
+# liczby zmiennoprzecinkowe - float
+print(float(1/3))
+print(0.1+0.1+0.1)
+print(0.1+0.1+0.1 == 0.3)
+from decimal import Decimal
+print(Decimal(0.3))
+#liczby zespolone: complex
+print(complex())
+print(10+11j)
+#wartości boolowskie: bool True False
+print(bool())
+print(10+True)
+#prześledzić działanie operatorów logicznych or and not
+print (True and False)
+print (not True)
