44c688728e9950cd26bca04d87134cc5a7c1cc59
ArkadiuszRolnik <arkadiusz.rolnik@gmail.com>
2019-10-28 20:30:03 +0100

    Commit

---
+++ Kolekcje/Sortowanie.py
@@ -0,0 +1,17 @@
+lista = [5, 10, 1, -1, 100, -34]
+
+print (lista)
+print (sorted(lista))
+print (lista)
+
+print (lista)
+print (lista.sort())
+print (lista)
+
+print (lista)
+print (lista.sort(reverse=True))
+print (lista)
+
+print("-"*40)
+zbiór = {5, 10, 1, -1, 100, -34}
+print(sorted(zbiór))
---
+++ Kolekcje/Zadanie_tabliczka.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    print(f"{i:5}", end = "")
+
+for i in range(10):
+    print(i, end="     ")
+    for j in range(10):
+        print(f"{i*j:3}", end ="")
+        print()
