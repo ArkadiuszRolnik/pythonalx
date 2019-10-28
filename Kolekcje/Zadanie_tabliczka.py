9ff7ee90894ac2104cc91e09d92f009be8776f69
ArkadiuszRolnik <arkadiusz.rolnik@gmail.com>
2019-10-28 20:33:04 +0100

    Commit

--- Kolekcje/Zadanie_tabliczka.py
+++ Kolekcje/Zadanie_tabliczka.py
@@ -1,8 +1,10 @@
+print("      ",end="")
 for i in range(10):
-    print(f"{i:5}", end = "")
-
+        print(f"{i:3}", end = "")
+print()
+print()
 for i in range(10):
     print(i, end="     ")
     for j in range(10):
         print(f"{i*j:3}", end ="")
-        print()
+    print()
